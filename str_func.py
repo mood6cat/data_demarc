cbfaf7dccb9181f747d029e9b7334fe18a5cc1fa
dant9 <dant982@mail.ru>
2023-03-30 15:13:33 +0500

    Create up_word

--- str_func.py
+++ str_func.py
@@ -1,7 +1,17 @@
 def up_words(word):
+    """
+    все буквы заглавные
+    :param word:
+    :return:
+    """
     return word.upper()
 
 def up_word(word):
+    """
+    каждое слово с заглавной буквы
+    :param word:
+    :return:
+    """
     return word.title()
 
 print(up_word("rrr fff"))
